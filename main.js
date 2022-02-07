8858a227b9abfc3d0614024096d6d578ee0a9676
waterboy94 <waterboy94@naver.com>
2022-02-07 13:52:58 +0900

    JS : Home Scroll and Opacity 1to0

--- main.js
+++ main.js
@@ -42,9 +42,19 @@ navbarLogo.addEventListener('click', (event) => {
 })
 
 
-//scroll기능을 중복으로 사용하기 때문에, 함수를 통해 중복코드 방지 처리
-//scrollIntoView라는 함수명이 중복되었지만, DOM요소에 정의한 함수와는 별개이기 때문에, 무한루프이슈는 발생하지 않는다.
+
+//scroll기능을 중복으로 사용하기 때문에, 함수를 통해 중복코드 방지 처리, 아래에 정의해놔도 hoisting된다.
+//scrollIntoView라는 함수명이 중복되었지만, DOM요소에 정의되어있는 함수와는 별개이기 때문에, 무한루프이슈는 발생하지 않는다.
 function scrollIntoView(selector) { //직접 정의한 함수
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth' }); //DOM요소에 정의한 함수
 }
+
+
+//Scroll을 아래로 내렸을때 Home 컨텐츠를 투명화(css opacity:1->0)하기
+const homeContainer = document.querySelector('.home__container');
+const homeDOMRect = homeContainer.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    homeContainer.style.opacity = 1 - window.scrollY / homeDOMRect;
+})
+
